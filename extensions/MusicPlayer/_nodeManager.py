0005a82c171f036f86d1ff807da4e8ec3c8687c1
Yuzuk1Shimotsuki <yuzuk1shimotsuki@lolicon.wtf>
2025-10-22 18:51:39 +0800

    chore: Code cleanup and optimization

--- extensions/MusicPlayer/_nodeManager.py
+++ extensions/MusicPlayer/_nodeManager.py
@@ -1,6 +1,6 @@
 import logging
 import os
-import pomice
+from pomice import NodePool
 from discord.ext import commands
 
 logger = logging.getLogger("music_v2")
@@ -8,12 +8,13 @@ logger = logging.getLogger("music_v2")
 class NodeManager:
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.pomice = pomice.NodePool()
+        self.nodePool = NodePool()
+        self.logger = self.bot.get_logger()
 
     async def start_nodes(self) -> None:
         """Initialize and start the Lavalink nodes."""
         try:
-            await self.pomice.create_node(
+            await self.nodePool.create_node(
                 bot=self.bot,
                 host=os.getenv("LAVALINK_HOST"),
                 port=int(os.getenv("LAVALINK_PORT")),
@@ -21,10 +22,10 @@ class NodeManager:
                 password=os.getenv("LAVALINK_PASSWORD"),
                 identifier="MAIN",
             )
-            logger.info("Pomice node created successfully.")
+            self.logger.info("Pomice node created successfully.")
         except Exception as exc:
-            logger.exception("Failed to create pomice node: %s", exc)
+            self.logger.exception("Failed to create pomice node: %s", exc)
 
     async def get_node_pool(self):
         """Get the node pool instance."""
-        return self.pomice
+        return self.nodePool
