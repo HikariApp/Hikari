0ad14ea4f1efcfb3f8e7fc9a4e0022bda84f577a
星野ゆき <hoshinoyuki@lolicon.io>
2025-10-25 19:03:14 +0800

    refactor: Rewriting Kick.py

--- moderation/Ban.py
+++ moderation/Ban.py
@@ -1,6 +1,6 @@
 from discord import Color, Embed, Forbidden, Member, User
 from discord.ext import commands
-from discord.ext.commands import Bot, Cog, Context, CommandInvokeError, MissingPermissions, BotMissingPermissions
+from discord.ext.commands import Bot, Cog, Context, CommandInvokeError, MissingPermissions, MissingRequiredArgument, BotMissingPermissions
 from typing import Optional
 
 class Ban(Cog):
@@ -123,25 +123,36 @@ class Ban(Cog):
     @ban.error
     async def ban_error(self, ctx: Context, error):
         embed = Embed(title="")
+        embed.color = Color.red()
+
+        if isinstance(error, MissingRequiredArgument) and error.param.name == "user":
+            # The command invoker doesn't provide the user argument
+            # A special case to return a more user-friendly message
+            embed.add_field(name="", value=f"Looks like you want me to **ban someone**, but **haven't specified** the user you would like to ban :thinking:  ...\nJust curious to know, **who** should I ban for now, {ctx.author.mention}?")
+            embed.color = ctx.author.color
+            return await ctx.send(embed=embed)
+        
+        if isinstance(error, MissingRequiredArgument):
+            # Missing argument(s)
+            embed.add_field(name="", value=f"<a:crossred:1356353067024515266> Missing argument: `{error.param.name}`. Please provide all required arguments, {ctx.author.mention}.")
+            return await ctx.send(embed=embed)
+
         if isinstance(error, MissingPermissions):
             # The command invoker doesn't have permissions
             embed.add_field(name="", value=f"<a:crossred:1356353067024515266> This command **requires** `ban_members` permission, and you probably **don't have** it, {ctx.author.mention}.")
+            return await ctx.send(embed=embed)
 
-        elif (
+        if (
             isinstance(error, BotMissingPermissions) or
             (isinstance(error, CommandInvokeError) and isinstance(error.original, Forbidden))    # Sometimes the application might throw a CommandInvokeError which caused by Forbidden, which is basically the same concept
         ):
             # The application doesn't have permissions to do so
             embed.add_field(name="", value=f"<a:crossred:1356353067024515266> I couldn't **ban** that user. Please **double-check** my **permissions** and **role position**.")
+            return await ctx.send(embed=embed)
 
-        else:
-            # Re-raise the error to prevent silent failures
-            raise error
-
-        embed.color = Color.red()
-        await ctx.send(embed=embed)
+        # If the error is not handled, re-raise the error to prevent silent failures
+        raise error
 
 
 async def setup(bot):
     await bot.add_cog(Ban(bot))
-
