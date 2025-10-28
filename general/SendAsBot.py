1b72dbe453bcad132ff93fad7d3b067cab5aa907
星野有希 <hoshinoyuki@lolicon.io>
2025-10-28 14:11:25 +0800

    refactor: safety guard to prevent unwanted mass mentions (spamming @everyone or @here) disasters in history in SendAsBot.py

--- general/SendAsBot.py
+++ general/SendAsBot.py
@@ -1,9 +1,10 @@
 import discord
-from discord import app_commands, Forbidden, Interaction
+from discord import app_commands, Forbidden, Interaction, TextStyle, VoiceChannel
 from discord.ext.commands import Bot, Cog
 from discord.ui import Modal, TextInput
-from discord.app_commands.errors import MissingPermissions, BotMissingPermissions
+from discord.app_commands.errors import MissingPermissions, BotMissingPermissions, CommandInvokeError
 from typing import Optional
+
 
 class SendAsBotModal(Modal):
     """
@@ -11,18 +12,31 @@ class SendAsBotModal(Modal):
     """
 
     content = TextInput(
-        label="Content",
-        style=discord.TextStyle.paragraph,
-        placeholder="Your content here...",
+        label="Content (Leave empty to send attachment only)",
+        style=TextStyle.paragraph,
+        placeholder="Any mass mentions like @everyone or @here will be sanitized unless you are the bot owner.",
         required=True,
         max_length=4000
     )
 
 
-    def __init__(self, silent: bool = False, file: Optional[discord.File] = None):
+    def __init__(self, bot: Bot, silent: bool = False, file: Optional[discord.File] = None):
+        self.bot = bot
         self.silent = silent
         self.file = file
         super().__init__(title="Send your message as bot's identity")
+
+
+    # Cog-level error listener for unhandled errors
+    async def cog_on_command_error(self, interaction: Interaction, error: Exception):
+        embed = discord.Embed(title="")
+
+        if isinstance(error, CommandInvokeError) and isinstance(error.original, Forbidden):
+            return await interaction.followup.send(embed=embed)
+        
+        self.logger.exception(f"Uncaught error in {interaction.cog.__cog_name__}:", exc_info=error)
+        raise error
+
 
     async def on_submit(self, interaction: Interaction):
         """
@@ -31,39 +45,57 @@ class SendAsBotModal(Modal):
         send_as_bot_error_embed = discord.Embed(title="", color=discord.Colour.red())
         await interaction.response.defer()
         content = self.content.value or None
+        #
+        # New safety guard to prevent unwanted mass mentions (spamming @everyone or @here) disasters in history.
+        # You can find it on (https://www.threads.com/@yukicon_/post/DQRQV0AEuQZ?xmt=AQF0BZjthgicIXxYGxVFI-rMDoMqo2fIOXRpvfqJrgNE4g) for more details.
+        #
+        # @everyone and @here mentions are now only available to bot owners
+        # For general users, this will replace both mentions with a zero-width space in between to prevent actual mentions
+        #
+        content = content if await self.bot.is_owner(interaction.user) else content.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere")
+
+        # Safety guard ends here, proceed to send the message
         if content is None and self.file is None:
             # Returns if both message and attachments are not provided
-            send_as_bot_error_embed.add_field(name="", value="<a:crossred:1356353067024515266> You cannot let me to send nothing! (say at least send a message or an attachment)", inline=False)
+            send_as_bot_error_embed.add_field(name="", value="<a:crossred:1356353067024515266> You cannot let me to send nothing... (say at least send a message or an attachment)", inline=False)
             return await interaction.followup.send(embed=send_as_bot_error_embed)
-        
-        if not isinstance(interaction.channel, discord.VoiceChannel):
-            try:
-                await interaction.channel.send(content=content, file=self.file, silent=self.silent)
     
-            except Forbidden as e:
-                if e.status == 403 and e.code == 50013:
-                    # Handling rare forbidden case
-                    send_as_bot_error_embed.add_field(name="", value=f"<a:crossred:1356353067024515266> I couldn't **send the content you provided**. Please **double-check** my **permissions** and **role position**.")
-                    return await interaction.followup.send(embed=send_as_bot_error_embed)
+        return await interaction.channel.send(content=content, file=self.file, silent=self.silent) if not isinstance(interaction.channel, VoiceChannel) else None
 
-                else:
-                    raise e
 
 class SendAsBot(Cog):
     def __init__(self, bot: Bot):
         global bool_value
         self.bot = bot
 
+
     # Send message from user input
-    @app_commands.command(description="Send your message or attatchment, or both as bot identity")
+    @app_commands.command(help="Send your message or attatchment, or both through me")
     @app_commands.checks.has_permissions(administrator=True)
     @app_commands.checks.bot_has_permissions(send_messages=True)
-    @app_commands.describe(silent="Send it as a silent message?")
-    @app_commands.describe(attachment="The attachment you would like to send. Leave this empty if you want to send the message only.")
     async def send(self, interaction: Interaction, silent: bool, attachment: Optional[discord.Attachment] = None):
+        """
+        Send your message or attatchment, or both through me
+
+        Parameters
+        ----------
+        interaction : `discord.Interaction`
+            The discord.Interaction object from the user.
+
+        silent : bool
+            Send it as a silent message?
+
+        attachment : `Optional[discord.Attachment]`
+            The attachment you would like to send. Leave this empty if you want to send the message only.
+
+        Returns
+        ----------
+        None
+
+        """
         # Converts the attachment to a discord.File() object
         file = await attachment.to_file() if attachment else None
-        await interaction.response.send_modal(SendAsBotModal(silent=silent, file=file))
+        await interaction.response.send_modal(SendAsBotModal(bot=self.bot, silent=silent, file=file))
 
 
     @send.error
